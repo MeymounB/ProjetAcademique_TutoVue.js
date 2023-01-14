b86ae1eb431399f6ee0a9db9ae906db1dcdacde1
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-01-14 17:42:42 +0100

    New project under project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <h1>Meymoun Boualaoui</h1>
   <p>Creative Developper</p>
-  <nav>
-    <router-link to="/">Accueil</router-link>
-    <router-link to="/projets">Projets</router-link>
-    <router-link to="/contact">Contact</router-link>
-  </nav>
-  <div class="trait"></div>
-  <div class="mainContent">
-    <p>Student in third year at Epsi, it has been now many years that I have interest in IT and
-      partciluray in Web Development.</p>
-    <p>For the moment, I prefer front-end (React/Vue) than back-end, but I'm
-      open-minded.</p>
-    <p>I'm actually looking for new experiences. Don't hesitate to contact me !</p>
-  </div>
+  <NavBar />
+  <Intros />
   <div class="trait"></div>
   <router-view />
   <br>
   <div class="trait"></div>
   <br>
   <footer>
-    <nav>
-      <div><router-link to="/">Accueil</router-link></div>
-      <div><router-link to="/projets">Projets</router-link></div>
-      <div><router-link to="/contact">Contact</router-link></div>
-    </nav>
+    <NavBar />
   </footer>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import Intros from "./components/Intros.vue";
+
+export default {
+  name: "app",
+  components: {
+    NavBar,
+    Intros,
+  },
+};
+</script>
 
 <style>
 /* .disappear {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+	<nav>
+		<router-link to="/">Accueil</router-link>
+		<router-link to="/projets">Projets</router-link>
+		<router-link to="/contact">Contact</router-link>
+	</nav>
+</template>
